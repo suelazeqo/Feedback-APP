93a868c555b8126445caaf620fd47e70c7cfd3fa
suelazeqo <suelazeqo19@gmail.com>
2025-03-04 10:53:37 +0100

    Simulating data retrieval from a server using JSON Server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,20 +1,24 @@
-import {createContext, useState} from "react";
+import {createContext, useState, useEffect} from "react";
 import {v4 as uuidv4} from "uuid";
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This is from Context',
-            rating: 8
-        }
-    ])
+    const [feedback, setFeedback] = useState([])
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
+
+    useEffect(()=>{
+        fetchFeedback()
+    })
+    const fetchFeedback = async ()=>{
+        const response = await fetch(
+            `http://localhost:5000/feedback?_sort=id&_order=desc`)
+        const data = await response.json();
+        setFeedback(data)
+    }
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
         setFeedback([newFeedback, ...feedback])
